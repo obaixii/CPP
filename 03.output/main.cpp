43e452904d29c8fd0ee0324d1fb949648e1c775c
obaixii <obaidawan567@gmail.com>
2024-05-12 21:08:30 +0500

    Lesson 3: Output

---
+++ 03.output/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>;
+using namespace std;
+
+int main()
+{
+    // C++ Output (Print Text)
+    cout << "Hello World!";
+    cout << "I am learning C++";
+    cout << "\n\n";
+    // C++ Output - New Lines
+    // To insert a new line, you can use the \n character:
+    cout << "Hello World! C++ from new line \n";
+    cout << "C++/CPP out";
+
+    // Two \n characters after each other will create a blank line:
+    cout << "Text After Blank Space \n\n";
+
+    // Another way to insert a new line, is with the endl manipulator:
+    cout << "Hello World! new line with endl manipulator" << endl;
+    return 0;
+}
+
+/*
+Both \n and endl are used to break lines. However, \n is most used.
+
+\n and endl - creates a line break
+\t - Creates a horizontal tab
+\\ - Inserts a backslash character (\)
+\" - Inserts a double quote character
+
+*/
