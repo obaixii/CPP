fde3d9c076d3a941371e303511eec66888880b21
obaixii <obaidawan567@gmail.com>
2024-05-14 00:21:07 +0500

    Lesson 4: Comments

---
+++ 04.comments/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Single Line comment
+    cout << "Hello World!\n\n";
+
+    cout << "Single Line Comments"; // Single Line comment
+
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    cout << "\n\nHello World! from Multiline Comment";
+}
